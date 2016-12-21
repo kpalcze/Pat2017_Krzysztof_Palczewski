ad6cd6a0209f98ecccc5d712b70cb12f0832be59
Krzysztof Palczewski <kpalcze@gmail.com>
2016-12-21 17:05:09 +0100

    home button

--- krzysztof.palczewski/app/src/main/java/zadania/krzysztofpalczewski/SplashActivity.java
+++ krzysztof.palczewski/app/src/main/java/zadania/krzysztofpalczewski/SplashActivity.java
@@ -9,28 +9,49 @@ public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 5000;
     private boolean backPressed = false;
+    private boolean onStopCalled = false;
+    private Handler handler;
+
+
+    //if BACK button wasn't pressed, after 5 seconds start MainActivity.java
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!backPressed){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
 
         //SplashActivity uses background_splash.xml as a theme background
 
-        //if BACK button wasn't pressed, after 5 seconds start MainActivity.java
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (!backPressed){
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+        //create handler launching MainActivity.java
+        handler = new Handler();
+        handler.postDelayed(runnable, SPLASH_DISPLAY_LENGTH);
+    }
 
-            }
-        }, SPLASH_DISPLAY_LENGTH);
+    //if onStart() is called again (e.g. user opens app that was in background state) create handler again
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (onStopCalled) {
+            handler = new Handler();
+            handler.postDelayed(runnable, SPLASH_DISPLAY_LENGTH / 2);
+        }
+    }
+
+    //if onStop() is called (e.g. user clicks HOME BUTTON) stop handler
+    @Override
+    protected void onStop() {
+        super.onStop();
+        onStopCalled = true;
+        handler.removeCallbacks(runnable);
     }
 
     @Override
